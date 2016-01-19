0a5ee67196cd4d645a540704180c4ec259406f68
Shan Rammah <shan.rammah@gmail.com>
2016-01-18 20:59:21 -0800

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Shan Rammah on 1/18/16.
+//  Copyright © 2016 Shan Rammah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by Shan Rammah on 1/18/16.
+//  Copyright © 2016 Shan Rammah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
